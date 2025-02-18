dea9aaa87d6341f769fd7fac38257383214b1c39
lucas123john <lucasjohn.edu@gmail.com>
2025-02-17 20:04:41 -0500

    Update brave_browser_command_handler.cc

--- browser/ui/webui/brave_browser_command/brave_browser_command_handler.cc
+++ browser/ui/webui/brave_browser_command/brave_browser_command_handler.cc
@@ -83,6 +83,9 @@ void BraveBrowserCommandHandler::CanExecuteCommand(
     case brave_browser_command::mojom::Command::kOpenAIChat:
       can_execute = CanShowAIChat(profile_);
       break;
+    case brave_browser_command::mojom::Command::kUpdateAdblockList:
+      can_execute = true;
+      break;
   }
   std::move(callback).Run(can_execute);
 }
@@ -113,6 +116,12 @@ void BraveBrowserCommandHandler::ExecuteCommand(
 #endif
     case brave_browser_command::mojom::Command::kOpenAIChat:
       delegate_->OpenAIChat();
+      break;
+
+    case brave_browser_command::mojom::Command::kUpdateAdblockList:
+      if (auto* adblock_service = brave::AdBlockFactory::GetForBrowserContext(profile_)) {
+      adblock_service->UpdateAdBlockLists();
+      }
       break;
   }
 
